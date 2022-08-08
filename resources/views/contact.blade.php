2d2305349f7fe8fccc8ddb0442b0f08dbfe8a961
Azwan Azlan <azwanazlan2809@gmail.com>
2022-08-08 21:26:18 +0800

    basic tutorial

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,12 @@
 
 </head>
 <body>
-<h1>This is contact page.</h1>
+    <h1>This is contact page from controller.</h1>
+    <a href="{{ route('about.page') }}"> About</a>
+
+    {{-- It is same as below. But the best pratice if use name instead of url
+    <a href="{{ url('/about') }}"> About</a> --}}
+
 </body>
 </html>
 
