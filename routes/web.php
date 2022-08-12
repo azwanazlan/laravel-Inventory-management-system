9db4d1258e66311d535f79012ce8e125c12b0670
Azwan Azlan <azwanazlan2809@gmail.com>
2022-08-12 17:45:47 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth,verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
