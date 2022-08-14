7bc9d821a56468b31a56dac35503c71f0ad87f25
azwanazlan <azwanazlan@gmail.com>
2022-08-14 14:45:42 +0800

    login with username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,17 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <!-- Email Address -->
+{{--             <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div> --}}
+
+            <div>
+                <x-label for="username" :value="__('Username')" />
+
+                <x-input id="username" class="block mt-1 w-full" type="username" name="username" :value="old('username')" required autofocus />
             </div>
 
             <!-- Password -->
