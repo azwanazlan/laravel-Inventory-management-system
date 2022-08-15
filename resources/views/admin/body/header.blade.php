c09195740d6a88939ddd3db828a3665de4087868
Azwan Azlan <azwanazlan2809@gmail.com>
2022-08-15 13:18:21 +0800

    fix error not load css

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -1,5 +1,3 @@
-
-
 <header id="page-topbar">
     <div class="navbar-header">
         <div class="d-flex">
@@ -7,19 +5,19 @@
             <div class="navbar-brand-box">
                 <a href="index.html" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm" height="22">
+                        <img src="assets/images/logo-sm.png" alt="logo-sm" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('backend/assets/images/logo-dark.png') }}" alt="logo-dark" height="20">
+                        <img src="assets/images/logo-dark.png" alt="logo-dark" height="20">
                     </span>
                 </a>
 
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm-light" height="22">
+                        <img src="assets/images/logo-sm.png" alt="logo-sm-light" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="logo-light" height="20">
+                        <img src="assets/images/logo-light.png" alt="logo-light" height="20">
                     </span>
                 </a>
             </div>
@@ -35,6 +33,137 @@
                     <span class="ri-search-line"></span>
                 </div>
             </form>
+
+            <div class="dropdown dropdown-mega d-none d-lg-block ms-2">
+                <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
+                    Mega Menu
+                    <i class="mdi mdi-chevron-down"></i>
+                </button>
+                <div class="dropdown-menu dropdown-megamenu">
+                    <div class="row">
+                        <div class="col-sm-8">
+
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <h5 class="font-size-14">UI Components</h5>
+                                    <ul class="list-unstyled megamenu-list">
+                                        <li>
+                                            <a href="javascript:void(0);">Lightbox</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Range Slider</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Sweet Alert</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Rating</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Forms</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Tables</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Charts</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <h5 class="font-size-14">Applications</h5>
+                                    <ul class="list-unstyled megamenu-list">
+                                        <li>
+                                            <a href="javascript:void(0);">Ecommerce</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Calendar</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Email</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Projects</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Tasks</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Contacts</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <h5 class="font-size-14">Extra Pages</h5>
+                                    <ul class="list-unstyled megamenu-list">
+                                        <li>
+                                            <a href="javascript:void(0);">Light Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Compact Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Horizontal layout</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Maintenance</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Coming Soon</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Timeline</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">FAQs</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <h5 class="font-size-14">UI Components</h5>
+                                    <ul class="list-unstyled megamenu-list">
+                                        <li>
+                                            <a href="javascript:void(0);">Lightbox</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Range Slider</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Sweet Alert</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Rating</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Forms</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Tables</a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);">Charts</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-sm-5">
+                                    <div>
+                                        <img src="assets/images/megamenu-img.png" alt="megamenu-img" class="img-fluid mx-auto d-block">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
 
         <div class="d-flex">
@@ -61,6 +190,10 @@
             </div>
 
             <div class="dropdown d-none d-sm-inline-block">
+                <button type="button" class="btn header-item waves-effect"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="" src="assets/images/flags/us.jpg" alt="Header Language" height="16">
+                </button>
                 <div class="dropdown-menu dropdown-menu-end">
 
                     <!-- item-->
@@ -85,7 +218,57 @@
                 </div>
             </div>
 
+            <div class="dropdown d-none d-lg-inline-block ms-1">
+                <button type="button" class="btn header-item noti-icon waves-effect"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="ri-apps-2-line"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
+                    <div class="px-lg-2">
+                        <div class="row g-0">
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/github.png" alt="Github">
+                                    <span>GitHub</span>
+                                </a>
+                            </div>
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/bitbucket.png" alt="bitbucket">
+                                    <span>Bitbucket</span>
+                                </a>
+                            </div>
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/dribbble.png" alt="dribbble">
+                                    <span>Dribbble</span>
+                                </a>
+                            </div>
+                        </div>
 
+                        <div class="row g-0">
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/dropbox.png" alt="dropbox">
+                                    <span>Dropbox</span>
+                                </a>
+                            </div>
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/mail_chimp.png" alt="mail_chimp">
+                                    <span>Mail Chimp</span>
+                                </a>
+                            </div>
+                            <div class="col">
+                                <a class="dropdown-icon-item" href="#">
+                                    <img src="assets/images/brands/slack.png" alt="slack">
+                                    <span>Slack</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="dropdown d-none d-lg-inline-block ms-1">
                 <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
@@ -185,7 +368,7 @@
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{ asset('backend/assets/images/users/avatar-1.jpg') }}"
+                    <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-1.jpg"
                         alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1">Julia</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
@@ -197,11 +380,15 @@
                     <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
                     <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="{{route('admin.logout')}}"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+                    <a class="dropdown-item text-danger" href="#"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
                 </div>
             </div>
 
-
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect">
+                    <i class="ri-settings-2-line"></i>
+                </button>
+            </div>
 
         </div>
     </div>
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.admin_master')
 @section('admin')
 
+
 <div class="page-content">
     <div class="container-fluid">
 
